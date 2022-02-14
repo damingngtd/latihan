0bf5b916b7a2af195b586bbd830cfc7e836a8441
damingngtd <ibrhmalnn@gmail.com>
2022-02-14 08:28:32 +0700

    menu profil sekolah

--- app/src/main/java/com/example/profilsekolah/BebasActivity.java
+++ app/src/main/java/com/example/profilsekolah/BebasActivity.java
@@ -1,13 +1,14 @@
 package com.example.profilsekolah;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class Latihan extends AppCompatActivity {
+public class BebasActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_latihan);
+        setContentView(R.layout.activity_bebas);
     }
 }
